cb18b1c169727d2e9a88bf4fc9283c07002d9716
hainguyenviet2305 <134821411+hainguyenviet2305@users.noreply.github.com>
2024-01-08 16:44:26 +0700

    add chart

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -5,6 +5,36 @@ import { changeState, changeTextWithIp } from "../reducer/tittleReducer";
 import Button from 'antd/es/button';
 import {changeNumber} from "../reducer/changeNumber";
 import { fetchUserById } from "../reducer/callApiReducer";
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
+import { Pie } from 'react-chartjs-2'
+
+ChartJS.register(ArcElement, Tooltip, Legend);
+export const dataChart = {
+  labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+  datasets: [
+    {
+      label: '# of Votes',
+      data: [12, 19, 3, 5, 2, 3],
+      backgroundColor: [
+        'rgba(255, 99, 132, 0.2)',
+        'rgba(54, 162, 235, 0.2)',
+        'rgba(255, 206, 86, 0.2)',
+        'rgba(75, 192, 192, 0.2)',
+        'rgba(153, 102, 255, 0.2)',
+        'rgba(255, 159, 64, 0.2)',
+      ],
+      borderColor: [
+        'rgba(255, 99, 132, 1)',
+        'rgba(54, 162, 235, 1)',
+        'rgba(255, 206, 86, 1)',
+        'rgba(75, 192, 192, 1)',
+        'rgba(153, 102, 255, 1)',
+        'rgba(255, 159, 64, 1)',
+      ],
+      borderWidth: 1,
+    },
+  ],
+};
 
 const HomePage = () => {
   const dispatch = useDispatch();
@@ -46,6 +76,8 @@ const HomePage = () => {
     <Button onClick={() => updateStatusTxt()}>change title</Button>
     ==========
       {console.log(dataRedux.responApi, 'table here')}
+
+      <Pie data={dataChart} />;
     </>;
   };
   
